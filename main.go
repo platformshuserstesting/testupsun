cd2a6fbf2fc17f93febdbde0d34bc70e127f7a2f
Mohan Prasath Sekar <mohan.sekar@platform.sh>
2026-06-03 12:37:43 +0530

    add main.go

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 	mergedResults := fanIn(worker1, worker2, worker3)
 
 	// 4. Consume results
-	for val := r mergedResults {
+	for val := range mergedResults {
 		fmt.Printf("Result Received: %d\n", val)
 	}
 }
