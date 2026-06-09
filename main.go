b26e40a79f4061d69ed023174a7dd45103d01d5a
platformshuserstesting <168089600+platformshuserstesting@users.noreply.github.com>
2026-06-09 23:45:48 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Stage 1: Generator - Produces data and sends it to a channels
-funcs generate(nums ...int) <-chan int {
+func generate(nums ...int) <-chan int {
 	outs := make(chan int)
 	go func() {
 		for _, n := range nums {
