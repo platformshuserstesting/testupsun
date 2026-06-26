33d40539ad93cddc7b7f67d3ac662ceadc1237dd
platformshuserstesting <168089600+platformshuserstesting@users.noreply.github.com>
2026-06-27 00:01:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func generate(nums ...int) <-chan int {
 	out := make(chan int)
 	go func() {
-		for _, n := range nums {
+		for _, n := range nums {s
 			out <- n
 		}
 		close(out) // Always close channels when done sending
